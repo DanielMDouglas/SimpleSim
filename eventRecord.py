02390fd35f490d9105694252cb6d4a1e3414bfd5
Daniel Douglas <dougl215@msu.edu>
2021-11-29 17:57:06 -0500

    added `eventRecord` object that contains intermediate data.  This is saved between steps using `numpy.save` for now

---
+++ eventRecord.py
@@ -0,0 +1,12 @@
+from utils import *
+from parameters import *
+
+class eventRecord:
+    def __init__(self):
+        self.pos = []
+        self.dir = []
+
+        self.qDepMap = []
+        self.chargeMap = []
+
+        self.hitMap = []
